4dc00f1c3f0159e53684cf36e4360fe5b4200764
vitorlana <vitorhugolana@hotmail.com>
2019-09-29 23:09:54 -0300

    to começando a entender

---
+++ rick_work/src/dados.h
@@ -0,0 +1,10 @@
+#ifndef DADOS_H
+#define DADOS_H
+
+typedef struct user_ent
+{
+    int quantidade;
+    char operacao;
+}Entrada;
+
+#endif
---
+++ rick_work/src/main.cpp
@@ -0,0 +1,34 @@
+//Bibliotecas Padrões
+
+#include <iostream>
+
+//Funções Específicas
+
+#include "dados.h"
+
+using namespace std;
+
+int main()
+{
+    Entrada user_ent;
+    while ((cin >> user_ent.quantidade >> user_ent.operacao))
+    {
+        if (user_ent.operacao = "i" or "I")
+        {
+            //Inserir recipiente
+            cout << "i \n";
+        }else if(user_ent.operacao = "r" or "R")
+        {   
+            //Remover recipiente
+            cout << "r \n";
+        }else if(user_ent.operacao = "p" or "P")
+        {
+            //Realizar medição
+            cout << "p \n";
+        }else if (cin.eof())
+        {
+            exit;
+        }else exit;
+    }
+    return 0;
+}
