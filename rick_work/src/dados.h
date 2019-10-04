88b241b71a7b8816a8b66e2c6006834b416b9f95
Vitor Lana <vitorhugolana@hotmail.com>
2019-10-04 15:05:25 -0300

    lista_quase_pronta

--- rick_work/src/dados.h
+++ rick_work/src/dados.h
@@ -1,10 +1,20 @@
 #ifndef DADOS_H
 #define DADOS_H
 
-typedef struct user_ent
+struct valor_celula
 {
     int quantidade;
     char operacao;
-}Entrada;
+};
+
+typedef struct lista_celula* Lista_celula;
+
+Lista_celula* cria_lista();
+bool lista_vazia(Lista_celula* lista);
+void libera_lista(Lista_celula* lista);
+int adiciona_celula(Lista_celula* lista, valor_celula Celula);
+void verifica_lista(Lista_celula* lista);
+void remove_celula(Lista_celula* lista, int a_quantidade);
+
 
 #endif
--- rick_work/src/main.cpp
+++ rick_work/src/main.cpp
@@ -10,25 +10,31 @@ using namespace std;
 
 int main()
 {
-    Entrada user_ent;
-    while ((cin >> user_ent.quantidade >> user_ent.operacao))
+
+    Lista_celula* lista;
+    
+    valor_celula celula_entrada;
+    lista = cria_lista();
+
+    while ((cin >> celula_entrada.quantidade >> celula_entrada.operacao))
     {
-        if (user_ent.operacao = "i" or "I")
+        if (celula_entrada.operacao == 'i') //Inserir recipiente
         {
-            //Inserir recipiente
-            cout << "i \n";
-        }else if(user_ent.operacao = "r" or "R")
+            adiciona_celula(lista, celula_entrada);
+        }else if(celula_entrada.operacao == 'r')
         {   
             //Remover recipiente
-            cout << "r \n";
-        }else if(user_ent.operacao = "p" or "P")
+            remove_celula(lista, celula_entrada.quantidade);
+        }else if(celula_entrada.operacao == 'p')
         {
             //Realizar medição
-            cout << "p \n";
+            verifica_lista(lista);
         }else if (cin.eof())
         {
             exit;
         }else exit;
     }
+
+    libera_lista(lista);
     return 0;
 }
