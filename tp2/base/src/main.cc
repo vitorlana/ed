f46a82ad19f51f53299920fed905739d10799889
Vitor Lana <vitorhugolana@hotmail.com>
2019-10-30 16:41:30 -0300

    rick_trip begin

---
+++ tp2/base/src/main.cc
@@ -0,0 +1,12 @@
+#include <cstdio> // em C substituir por #include <stdio.h>
+#include "foo.h"
+
+int main() {
+  int t;
+  int p;
+  int c;
+  while(scanf("%d %d %d", &t, &p, &c) != EOF) {
+      return 1;
+  }
+  return 0;
+}
---
+++ tp2/rick_trip/src/foo.h
@@ -0,0 +1,6 @@
+#ifndef __TP1_ED___
+#define __TP1_ED__
+
+int bar();
+
+#endif
